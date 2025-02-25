7be6e09b74ca76d35f65d53d70a7544c5368dac4
consolelog <leopportunity5@gmail.com>
2025-02-25 09:05:29 +0900

    update: seperate data code

--- base/gpa_model.py
+++ base/gpa_model.py
@@ -1,41 +1,10 @@
 import os
-import numpy as np 
-import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-import pandas as pd
-from torch.utils.data import Dataset, DataLoader
+from gpa_data import dl 
 
 root_path = f"{os.getcwd()}/base"
-
-class GPADataset(Dataset):
-    def __init__(self, path):
-        super(GPADataset, self).__init__()
-        self.data = pd.read_csv(path)
-        self.data.dropna(inplace=True)
-        self.x = self.data[["gre", "gpa", "rank"]].to_numpy() 
-        self.y = self.data["admit"].to_numpy() 
-    
-    def __getitem__(self, idx):
-        return torch.tensor(self.x[idx]), torch.tensor(self.y[idx]) 
-    
-    def __len__(self):
-        return len(self.data)
-
-gpa_dataset = GPADataset(f"{root_path}/data/gpascore.csv")
-
-def collater(batch):
-    X, y = [], []
-    for scores, admit in batch: 
-        X.append(scores)
-        y.append(admit)  
-    X = torch.tensor(np.array(X), dtype=torch.float32)
-    y = torch.tensor(np.array(y).reshape(-1, 1), dtype=torch.float32)
-    return X, y    
-
-
-dl = DataLoader(gpa_dataset, batch_size=8, shuffle=True, collate_fn=collater)
 
 class GPAModel(nn.Module):
     def __init__(self, input_dim):
